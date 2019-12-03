b5d4684d5c2775b7ecad3844a974644f35ae4679
Steve Shaffer <steve@shaffer.tech>
2019-12-02 22:45:04 -0800

    .

--- test/happy-path.sh
+++ test/happy-path.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PATH="${DIR}/../bin:$PATH"
+
 # I think if one of these fails, it'll just move on to the next one,
 # so either way you should get a random string at the end of it.
 RANDOM_STRING=$(date | md5 | head -c8; echo)  # Works on OSX
@@ -14,6 +17,10 @@ cd temp
 ## TODO: Boil down to `gish open gish-test`
 git clone https://github.com/SteveShaffer/gish-test
 cd gish-test
+
+# Need to configure git user
+git config user.email "test@shaffer.tech"
+git config user.name "test account"
 
 # Testing commit and pr
 gish switch test-commit/${RANDOM_STRING}
